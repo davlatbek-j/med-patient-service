239bbdecd37a6c569c40279e55490ee7e4bf4af6
SuperHuma <theferganes@gmail.com>
2024-05-29 15:29:53 +0500

    Initial commit

---
+++ src/main/java/uz/imed/MedPatientServiceApplication.java
@@ -0,0 +1,13 @@
+package uz.imed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedPatientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedPatientServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/imed/MedPatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.imed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedPatientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
