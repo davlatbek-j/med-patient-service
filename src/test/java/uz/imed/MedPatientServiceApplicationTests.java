9bd3c5096a811dd37beaa4ca3a1dad8717b37a86
SuperHuma <theferganes@gmail.com>
2024-05-29 15:30:15 +0500

    com-1

--- src/test/java/uz/imed/MedPatientServiceApplicationTests.java
+++ src/test/java/uz/imed/MedPatientServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MedPatientServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
