b1c94426cb9bb6fa28d885ed69dc627b50c1d25b
SuperHuma <theferganes@gmail.com>
2024-05-29 18:44:14 +0500

    com-1

--- src/main/java/uz/imed/MedPatientServiceApplication.java
+++ src/main/java/uz/imed/MedPatientServiceApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MedPatientServiceApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(MedPatientServiceApplication.class, args);
     }
 
